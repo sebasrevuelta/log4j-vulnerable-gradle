da54798860315a9acf7236e0d19a12afdddc8207
sebastianrevuelta <sebastianrevuelta@gmail.com>
2023-01-30 12:38:13 +0100

    add verbose

--- src/main/java/HelloLog.java
+++ src/main/java/HelloLog.java
@@ -2,6 +2,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Level;
 
+//Test class
 public class HelloLog {
 
     private static final Logger logger = LogManager.getLogger();
