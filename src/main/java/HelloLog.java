0ed283f0308bb327323077bc6e431586accbb0da
Alexis Grant <alexis@r2c.dev>
2024-07-03 12:15:04 -0700

    add todo comment

--- src/main/java/HelloLog.java
+++ src/main/java/HelloLog.java
@@ -2,7 +2,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Level;
 
-//Test class
+// TODO: Test class
 public class HelloLog {
 
     private static final Logger logger = LogManager.getLogger();
